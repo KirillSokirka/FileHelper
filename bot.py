7c444bea76ac361cddc8fe6abe3c6f878d9bf2a1
Kirill Sokirka <72627972+KirillSokirka@users.noreply.github.com>
2021-11-25 22:13:52 +0200

    Create bot, upload to heroku

---
+++ bot.py
@@ -0,0 +1,39 @@
+import os
+
+import telebot
+
+from configs.config import BOT_TOKEN, APP_URL
+
+from telebot import TeleBot
+from flask import request, Flask
+
+app = Flask(__name__)
+bot = TeleBot(BOT_TOKEN)
+
+
+@bot.message_handler(content_types='text')
+def echo(message: telebot.types.Message):
+    bot.send_message(message.from_user.id, message.text)
+
+
+@app.route('/' + BOT_TOKEN, methods=['POST'])
+def get_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "Ok", 200
+
+
+@app.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return "Ok", 200
+
+
+if __name__ == '__main__':
+    if os.getenv('Heroku'):
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    else:
+        bot.remove_webhook()
+        bot.polling(none_stop=True)
---
+++ configs/config.py
@@ -0,0 +1,3 @@
+
+BOT_TOKEN = '2122566110:AAGdLMxuiyO4NYv3cTPKqrZFQmBmhCclvg8'
+APP_URL = 'https://filehelper.herokuapp.com/' + BOT_TOKEN
