6f61dc796b8a771ff1ee263fad2e8d32d4f8671d
Kirill Sokirka <k.sokirka-2024@kpi.ua>
2021-11-28 21:06:56 +0200

    Add translation part

--- Models/TranslationDTO.py
+++ Models/TranslationDTO.py
@@ -5,3 +5,34 @@ class TranslationDTO:
         self.file_path = ''
         self.source_lan = ''
         self.dest_lan = ''
+
+    @property
+    def file_path(self):
+        return self.__file_path
+
+    @file_path.setter
+    def file_path(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        self.__file_path = value
+
+    @property
+    def source_lan(self):
+        return self.__source_lan
+
+    @source_lan.setter
+    def source_lan(self, value):
+        if not value:
+            if not isinstance(value, str):
+                raise TypeError
+        self.__source_lan = value
+
+    @property
+    def dest_lan(self):
+        return self.__dest_lan
+
+    @dest_lan.setter
+    def dest_lan(self, value):
+        if not isinstance(value, str):
+            raise TypeError
+        self.__dest_lan = value
